addba8c07390305ae94cad41600937834396a01d
Coltan7980 <te1976by@go.minnstate.edu>
2025-01-30 15:14:31 -0600

    Initial Commit
    Where you would comment what section your starting , where your at on it, and where you plan to go with it

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Practice_File
+{
+    internal class TemperatureCalculator
+    {
+        private double farenheight = -999.9;
+
+        public double Farenheight
+        {
+            get { return this.farenheight; }
+            set { this.farenheight = value; }
+        }
+
+        public double CalcCelsius()
+        {
+            return (Farenheight - 32.0) * (5.0/9.0);
+        }
+
+        public double CalcKelvin()
+        {
+            return CalcCelsius() + 273.15;
+        }
+        public double CalcRankine()
+        {
+            return Farenheight + 45967.0;
+        }
+        public override string ToString()
+        {
+            string msg = "";
+            msg = msg + "In Celcius :" + CalcCelsius() + "\n";
+            msg = msg + "In Kelvin :" + CalcKelvin() + "\n";
+            msg = msg + "In Rankine :" + CalcRankine() + "\n";
+
+            return msg;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Practice_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           TemperatureCalculator temperatureCalculator = new TemperatureCalculator();
+            
+            temperatureCalculator.Farenheight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(temperatureCalculator.ToString());
+        }
+    }
+}
